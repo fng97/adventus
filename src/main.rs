696b2eefea6f90fae08f9c2b4ec4accc7a4a8f21
Francisco <francisco@tended.co.uk>
2024-03-17 19:04:20 +0000

    Gets bot playing a chime when you join.
    When a user joins a voice channel in a guild the bot is in:
    
    1. bot joins the voice channel
    2. bot plays a chime
    
    If another user joins the same channel while the bot is playing, it will
    keep playing the original chime and overlay the new chime on top of it.
    
    If another user joins a different channel, the bot will join the new
    channel and play the chime regardless of whether or not the bot was
    playing.

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,22 @@
-//! Requires the "client", "standard_framework", and "voice" features be enabled in your
-//! Cargo.toml, like so:
-//!
-//! ```toml
-//! [dependencies.serenity]
-//! git = "https://github.com/serenity-rs/serenity.git"
-//! features = ["client", "standard_framework", "voice"]
-//! ```
-use std::env;
-
-// This trait adds the `register_songbird` and `register_songbird_with` methods
-// to the client builder below, making it easy to install this voice client.
-// The voice client can be retrieved in any command using `songbird::get(ctx).await`.
-use songbird::SerenityInit;
-
-// Event related imports to detect track creation failures.
-use songbird::events::{Event, EventContext, EventHandler as VoiceEventHandler, TrackEvent};
-
-// To turn user URLs into playable audio, we'll use yt-dlp.
-use songbird::input::YoutubeDl;
-
-// YtDl requests need an HTTP client to operate -- we'll create and store our own.
 use reqwest::Client as HttpClient;
-
-// Import the `Context` to handle commands.
-use serenity::client::Context;
-
 use serenity::{
     async_trait,
-    client::{Client, EventHandler},
-    framework::standard::CommandResult,
+    client::{Client, Context, EventHandler},
     model::{gateway::Ready, voice::VoiceState},
     prelude::{GatewayIntents, TypeMapKey},
 };
+use songbird::events::{Event, EventContext, EventHandler as VoiceEventHandler, TrackEvent};
+use songbird::input::YoutubeDl;
+use songbird::SerenityInit;
+use std::env;
 
-const SONG_URL: &str = "https://www.youtube.com/watch?v=NCtzkaL2t_Y";
+const SONG_URL: &str = "https://www.youtube.com/watch?v=V66PMeImkxI";
 
 struct HttpKey;
 
 impl TypeMapKey for HttpKey {
     type Value = HttpClient;
 }
-
-async fn connect_voice(
-    ctx: &Context,
-    guild_id: serenity::model::id::GuildId,
-    channel_id: serenity::model::id::ChannelId,
-) -> CommandResult {
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-
-    if let Ok(handler_lock) = manager.join(guild_id, channel_id).await {
-        // Attach an event handler to see notifications of all track errors.
-        let mut handler = handler_lock.lock().await;
-        handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
-    }
-
-    Ok(())
-}
-
-async fn play_sound(
-    ctx: &Context,
-    url: String,
-    guild_id: serenity::model::id::GuildId,
-) -> CommandResult {
-    let http_client = {
-        let data = ctx.data.read().await;
-        data.get::<HttpKey>()
-            .cloned()
-            .expect("Guaranteed to exist in the typemap.")
-    };
-
-    let manager = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .clone();
-
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let mut handler = handler_lock.lock().await;
-
-        let src = YoutubeDl::new(http_client, url);
-        let _ = handler.play_input(src.clone().into());
-
-        println!("Playing song");
-    } else {
-        println!("Not in a voice channel to play in");
-    }
-
-    Ok(())
-}
-
 struct Handler;
 
 #[async_trait]
@@ -100,22 +26,52 @@ impl EventHandler for Handler {
     }
 
     async fn voice_state_update(&self, ctx: Context, _old: Option<VoiceState>, new: VoiceState) {
-        if let Some(guild_id) = new.guild_id {
-            if let Some(channel_id) = new.channel_id {
-                // Assuming you want to play music only when a specific user (e.g., yourself) joins a voice channel.
-                // You should replace `YOUR_USER_ID` with your actual Discord user ID.
-                // if new.user_id.to_string() == "YOUR_USER_ID" {
-                // Connect to the voice channel
-                if let Err(e) = connect_voice(&ctx, guild_id, channel_id).await {
-                    println!("Error connecting to voice channel: {:?}", e);
-                }
-                // Play the sound
-                if let Err(e) = play_sound(&ctx, SONG_URL.to_string(), guild_id).await {
-                    println!("Error playing sound: {:?}", e);
-                }
-                // }
-            }
+        // Early return if there's no guild_id
+        let guild_id = match new.guild_id {
+            Some(guild_id) => guild_id,
+            None => return,
+        };
+
+        // Early return if the user joining the channel is the bot itself
+        if new.user_id == ctx.cache.current_user().id {
+            println!("Bot joined a channel");
+            return;
         }
+
+        // Early return if there's no channel_id
+        let channel_id = match new.channel_id {
+            Some(channel_id) => channel_id,
+            None => return,
+        };
+
+        // Proceed with joining the channel and setting up the environment
+        let manager = songbird::get(&ctx)
+            .await
+            .expect("Songbird Voice client placed in at initialisation.")
+            .clone();
+
+        // Attempt to join the voice channel, early return on failure
+        let handler_lock = match manager.join(guild_id, channel_id).await {
+            Ok(handler_lock) => handler_lock,
+            Err(_) => return,
+        };
+
+        let mut handler: tokio::sync::MutexGuard<'_, songbird::Call> = handler_lock.lock().await;
+
+        // Attach an event handler to see notifications of all track errors.
+        handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
+
+        // Get the HTTP client, required by YoutubeDl
+        let http_client = {
+            let data = ctx.data.read().await;
+            data.get::<HttpKey>()
+                .cloned()
+                .expect("Guaranteed to exist in the typemap.")
+        };
+
+        // Play
+        let src = YoutubeDl::new(http_client, SONG_URL.to_string());
+        let _ = handler.play_input(src.clone().into());
     }
 }
 
@@ -123,7 +79,6 @@ impl EventHandler for Handler {
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     let intents = GatewayIntents::non_privileged()
@@ -133,12 +88,7 @@ async fn main() {
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .register_songbird()
-        // We insert our own HTTP client here to make use of in
-        // `~play`. If we wanted, we could supply cookies and auth
-        // details ahead of time.
-        //
-        // Generally, we don't want to make a new Client for every request!
-        .type_map_insert::<HttpKey>(HttpClient::new())
+        .type_map_insert::<HttpKey>(HttpClient::new()) // shared HTTP client for YoutubeDl
         .await
         .expect("Err creating client");
 
