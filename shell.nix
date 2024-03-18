ecd6a2fd471d6655642a889aa045c4b2c2332087
Francisco <53615823+fng97@users.noreply.github.com>
2024-03-18 14:57:00 +0000

    Adds Nix dev shell.

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.rustc # rust deps
+    pkgs.cargo # rust deps
+
+    pkgs.cargo-shuttle # infrastructure
+
+    pkgs.darwin.apple_sdk.frameworks.SystemConfiguration # for shuttle
+
+    pkgs.nixpkgs-fmt # for nix formatting
+
+    pkgs.sqlx-cli # for test database
+    pkgs.docker # for test database
+    pkgs.postgresql # for test database
+
+    pkgs.shfmt # vscode shell formatting dep
+    pkgs.libiconv # shuttle dep
+    pkgs.yt-dlp # songbird youtube downloader dep
+  ];
+
+  shellHook = ''
+    echo "Development environment loaded."
+  '';
+}
