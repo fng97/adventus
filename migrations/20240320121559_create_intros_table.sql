f428f59bcd38702401663c528fc0a2fb372641f4
Francisco <francisco@tended.co.uk>
2024-03-20 16:54:31 +0000

    Adds timestamp to schema.

--- migrations/20240320121559_create_intros_table.sql
+++ migrations/20240320121559_create_intros_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE intros (
     user_snowflake BIGINT NOT NULL,
     guild_snowflake BIGINT NOT NULL,
     yt_url text NOT NULL,
+    set_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (user_snowflake, guild_snowflake)
 );
