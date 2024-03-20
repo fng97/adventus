a7a0d35c474be4b7e3987cd62181e8fcb5429f75
Francisco <francisco@tended.co.uk>
2024-03-20 16:47:57 +0000

    Handles more dependencies in dev container config.
    Adds extra dependencies:
    
    - sqlx-cli: for working with postgres
    - shfmt: for the shfmt extension
    - yt-dlp: use feature instead of pip
    - postgresql-client: for working with postgres
    
    Adds extra extensions:
    
    - sqltools: for working with sql databases
    - sqltools-driver-pg: postgres extension for above
    
    No longer have to use pip.

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-apt-get update && apt-get install -y cmake python3-venv
-
-python3 -m venv .venv
-source .venv/bin/activate
-
-pip install --upgrade pip
-pip install yt-dlp
+apt-get update && apt-get install -y cmake postgresql-client
