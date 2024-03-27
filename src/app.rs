758a7fde4ebe54044b9b463baae32a0c976e32fe
Francisco <53615823+fng97@users.noreply.github.com>
2024-03-27 10:35:38 +0000

    Default dice roll to most common: 1d20.

--- src/app.rs
+++ src/app.rs
@@ -50,23 +50,31 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
 /// Roll the dice!
+///
+/// This command rolls the given number of dice with the given number of sides.
+/// The number of sides is 20 by default. The number of dice is optional and
+/// defaults to 1. The number of sides must be between 2 and 100, and the number
+/// of dice must be between 1 and 10.
 #[poise::command(slash_command)]
 pub async fn roll(
     ctx: Context<'_>,
     #[description = "Number of sides of the dice."]
     #[min = 2]
     #[max = 100]
-    sides: u8,
+    sides: Option<u8>,
     #[description = "Number of dice to roll."]
     #[min = 1]
     #[max = 10]
     rolls: Option<u8>,
 ) -> Result<(), Error> {
     const DEFAULT_NUM_ROLLS: u8 = 1;
+    const DEFAULT_NUM_SIDES: u8 = 20;
+
     let rolls = rolls.unwrap_or(DEFAULT_NUM_ROLLS);
+    let sides = sides.unwrap_or(DEFAULT_NUM_SIDES);
 
     let results: Vec<u8> = (0..rolls)
-        .map(|_| rand::thread_rng().gen_range(1..sides))
+        .map(|_| rand::thread_rng().gen_range(1..=sides))
         .collect();
 
     // Create a string of the results to send in the message
