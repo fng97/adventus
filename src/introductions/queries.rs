3d4b61f1b7dce3d409d1fe3b83b258c79cfb3c2d
Francisco <53615823+fng97@users.noreply.github.com>
2024-03-28 15:00:10 +0000

    Adds more tests to introductions queries.
    Includes testing for multiple combinations of guilds and users, clearing
    set entries, and overwriting set entries.

--- src/introductions/queries.rs
+++ src/introductions/queries.rs
@@ -119,27 +119,89 @@ mod tests {
     use super::*;
     use crate::database_setup::test_utils::get_test_database;
 
-    #[tokio::test]
-    async fn get_url_returns_set_url() {
-        // arrange
-        const USER_SNOWFLAKE: u64 = 123456789123456789;
-        const GUILD_SNOWFLAKE: u64 = 987654321987654321;
-        const URL: &str = "https://example.com";
+    const USER_SNOWFLAKE_1: u64 = 123456789123456789;
+    const USER_SNOWFLAKE_2: u64 = 987654321987654321;
+    const GUILD_SNOWFLAKE_1: u64 = 9876543219876543210;
+    const GUILD_SNOWFLAKE_2: u64 = 1234567891234567890;
+    const URL_1: &str = "https://example.com";
+    const URL_2: &str = "https://example2.com";
 
+    #[tokio::test]
+    async fn set_urls_are_retrieved() {
+        // arrange
+        let connection_pool = get_test_database().await;
+        let test_cases = vec![
+            (USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, URL_1),
+            (USER_SNOWFLAKE_2, GUILD_SNOWFLAKE_1, URL_2),
+            (USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_2, URL_2),
+            (USER_SNOWFLAKE_2, GUILD_SNOWFLAKE_2, URL_1),
+        ];
+
+        for (user_snowflake, guild_snowflake, url) in test_cases {
+            // act
+            set_url_for_user_and_guild(user_snowflake, guild_snowflake, url, &connection_pool)
+                .await
+                .unwrap();
+            let expected_url =
+                get_url_for_user_and_guild(user_snowflake, guild_snowflake, &connection_pool)
+                    .await
+                    .unwrap();
+
+            // assert
+            assert_eq!(expected_url, url);
+        }
+    }
+
+    #[tokio::test]
+    async fn get_url_returns_none_when_no_url_set() {
+        // arrange
         let connection_pool = get_test_database().await;
 
-        set_url_for_user_and_guild(USER_SNOWFLAKE, GUILD_SNOWFLAKE, URL, &connection_pool)
+        // act
+        let expected_url =
+            get_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, &connection_pool).await;
+
+        // assert
+        assert!(expected_url.is_none());
+    }
+
+    #[tokio::test]
+    async fn set_url_overwrites_existing_url() {
+        let connection_pool = get_test_database().await;
+        set_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, URL_1, &connection_pool)
+            .await
+            .unwrap();
+        set_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, URL_2, &connection_pool)
             .await
             .unwrap();
 
         // act
-
         let expected_url =
-            get_url_for_user_and_guild(USER_SNOWFLAKE, GUILD_SNOWFLAKE, &connection_pool)
+            get_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, &connection_pool)
                 .await
                 .unwrap();
 
         // assert
-        assert_eq!(expected_url, URL);
+        assert_eq!(expected_url, URL_2);
+    }
+
+    #[tokio::test]
+    async fn clear_url_removes_url() {
+        // arrange
+        let connection_pool = get_test_database().await;
+        set_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, URL_1, &connection_pool)
+            .await
+            .unwrap();
+
+        // act
+        clear_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, &connection_pool)
+            .await
+            .unwrap();
+
+        let expected_url =
+            get_url_for_user_and_guild(USER_SNOWFLAKE_1, GUILD_SNOWFLAKE_1, &connection_pool).await;
+
+        // assert
+        assert!(expected_url.is_none());
     }
 }
