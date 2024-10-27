bf95948456ef4cffaa04e3c44d0b36449a4b8a62
fng97 <53615823+fng97@users.noreply.github.com>
2024-10-27 15:30:20 +0000

    Add nix development flake.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.stable.latest.default
+
+            cargo-edit  # for `cargo upgrade`
+
+            # rust-bin.nightly.latest.default
+            # cargo-udeps
+          ];
+        };
+      }
+    );
+}
