54d6682175374442fd21085f7d02ec2fdb7eeab0
fng97 <53615823+fng97@users.noreply.github.com>
2025-02-23 10:01:46 +0000

    Add lots of logging to websocket server

--- ws_echo.py
+++ ws_echo.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
-"""Echo server using the threading API."""
+"""WebSocket echo server using the threading API."""
 
 from websockets.sync.server import serve
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def echo(websocket):
-    for message in websocket:
-        websocket.send(message)
+    logger.debug(f"Client at {websocket.remote_address} successfully connected")
+    try:
+        for message in websocket:
+            logger.info(f"Received message: {message}")
+            websocket.send(message)
+    except Exception as e:
+        logger.error(f"Error handling connection: {e}")
 
 
 HOST = "localhost"
@@ -15,8 +24,9 @@ PORT = 8765
 
 
 def main():
-    print(f"WebSockets echo server at {HOST}:{PORT}")
-    with serve(echo, HOST, PORT) as server:
+    logging.basicConfig(level=logging.DEBUG)
+    logger.info(f"Launching WebSockets echo server at ws://{HOST}:{PORT}")
+    with serve(echo, HOST, PORT, logger=logging.getLogger()) as server:
         server.serve_forever()
 
 
