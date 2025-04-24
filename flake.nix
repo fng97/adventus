07e87c4b2e6aec58eb976c53f0a275c9cd25a450
fng97 <fng97@icloud.com>
2025-04-24 07:26:06 +0000

    flake: Add libopus runtime dependency

--- flake.nix
+++ flake.nix
@@ -28,14 +28,13 @@
         default = pkgs.mkShell {
           packages = with pkgs;
             [
-              cmake
               rustToolchain
               openssl
               pkg-config
               cargo-edit
               rust-analyzer
               ffmpeg
-              # libopus # ?
+              libopus
             ] ++ lib.optionals pkgs.stdenv.isDarwin
             [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
 
