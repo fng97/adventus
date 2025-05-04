fb6b004b09a856d7343dd068b2f5e4c5fa251164
fng97 <fng97@icloud.com>
2025-05-04 09:12:04 +0100

    flake: Add Nix build and shell

--- flake.nix
+++ flake.nix
@@ -1,48 +1,42 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { nixpkgs, ... }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays =
-                [ rust-overlay.overlays.default self.overlays.default ];
-            };
-          });
-    in {
-      overlays.default = final: prev: {
-        rustToolchain = prev.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rustfmt" ];
-        };
-      };
+      # boilerplate for cross-platform builds/shells
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              rustToolchain
-              openssl
-              pkg-config
-              cargo-edit
-              rust-analyzer
-              ffmpeg
-              libopus
-            ] ++ lib.optionals pkgs.stdenv.isDarwin
+      # function to create package for each supported system
+      makePackage = system:
+        let
+          pkgs = pkgsFor.${system};
+          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        in pkgs.rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          src = pkgs.lib.cleanSource ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          # build-time dependencies
+          nativeBuildInputs = with pkgs;
+            [ pkg-config ] ++ lib.optionals pkgs.stdenv.isDarwin
             [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
-
-          env = {
-            RUST_SRC_PATH =
-              "${pkgs.rustToolchain}/lib/rustlib/src/rust/library"; # required by rust-analyzer
-          };
+          # run-time dependencies
+          buildInputs = with pkgs; [ openssl ffmpeg libopus ];
         };
-      });
+
+      # function to create dev shell for each supported system
+      makeDevShell = system:
+        let
+          pkgs = pkgsFor.${system};
+          rustPackage = makePackage system;
+        in pkgs.mkShell {
+          inputsFrom = [ rustPackage ];
+          buildInputs = with pkgs; [ rustc cargo rust-analyzer cargo-edit ];
+        };
+    in {
+      packages = forAllSystems (system: { default = makePackage system; });
+      devShells = forAllSystems (system: { default = makeDevShell system; });
     };
 }
